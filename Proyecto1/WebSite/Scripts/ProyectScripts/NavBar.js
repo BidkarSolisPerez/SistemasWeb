aa2b387d23b60b800ee4cf0e566e32cff9f9153a
BidkarSolisPerez <bidkr.solis@gmail.com>
2019-08-18 21:52:45 -0600

    Agregando Bootstrap
    Agregando Bootstrap AL PROYECTO

---
+++ Proyecto1/WebSite/Scripts/ProyectScripts/NavBar.js
@@ -0,0 +1,4 @@
+﻿$('#navigation .navbar-nav .nav-link a').on('click', function () {
+    $('#navigation .navbar-nav .nav-link').find('li.active').removeClass('active');
+    $(this).parent('li').addClass('active');
+});
