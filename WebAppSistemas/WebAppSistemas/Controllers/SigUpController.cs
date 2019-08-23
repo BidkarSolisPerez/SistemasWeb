f0f27911f0533982f18f8b57e200ebb76ad33628
BidkarSolisPerez <bidkr.solis@gmail.com>
2019-08-23 16:30:37 -0600

    Adding html
    Adding html

---
+++ WebAppSistemas/WebAppSistemas/Controllers/SigUpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppSistemas.Controllers
+{
+    public class SigUpController : Controller
+    {
+        // GET: SigUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
