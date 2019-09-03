3dd03b0950d02d95c8cfac44784ee1890eb6f4ea
CarlosArguedas <carguedas17@hotmail.com>
2019-09-02 23:33:31 -0600

    Proyecto1 version final con SQLExpress

---
+++ Proyecto1 - FinalGit/Proyecto1/Controllers/ContactUsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Proyecto1.Models;
+
+namespace Proyecto1.Controllers
+{
+    public class ContactUsController : Controller
+    {
+        private Proyecto1Entities db = new Proyecto1Entities();
+
+        // GET: ContactUs
+        public ActionResult Index()
+        {
+            return View(db.ContactUs.ToList());
+        }
+
+        // GET: ContactUs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactUs contactUs = db.ContactUs.Find(id);
+            if (contactUs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactUs);
+        }
+
+        // GET: ContactUs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ContactUs/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Email,Comments")] ContactUs contactUs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContactUs.Add(contactUs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactUs);
+        }
+
+        // GET: ContactUs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactUs contactUs = db.ContactUs.Find(id);
+            if (contactUs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactUs);
+        }
+
+        // POST: ContactUs/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Email,Comments")] ContactUs contactUs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactUs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contactUs);
+        }
+
+        // GET: ContactUs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ContactUs contactUs = db.ContactUs.Find(id);
+            if (contactUs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactUs);
+        }
+
+        // POST: ContactUs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ContactUs contactUs = db.ContactUs.Find(id);
+            db.ContactUs.Remove(contactUs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
